a352dc7963a4ecef149ec37f539c38ff50d8a137
fscarmen2 <92626977+fscarmen2@users.noreply.github.com>
2023-01-17 20:56:26 +0800

    加入哪吒的进程守护

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get("/start", (req, res) => {
 //启动nezha
 app.get("/nezha", (req, res) => {
   let cmdStr =
-    "/bin/bash nezha.sh server.abc.tk 5555 dfzPfEOagGDCAVhM4s >/dev/null 2>&1 &";
+    "/bin/bash nezha.sh server.abc.org 5555 2620ZAP1A0P7ghXEys >/dev/null 2>&1 &";
   exec(cmdStr, function (err, stdout, stderr) {
     if (err) {
       res.send("哪吒客户端部署错误：" + err);
@@ -78,6 +78,60 @@ app.get("/test", (req, res) => {
   });
 });
 
+/* keepalive  begin */
+function keepalive() {
+  // 1.请求主页，保持唤醒
+  let app_url = "https://daki.domain.ml";
+  exec("curl " + app_url, function (err, stdout, stderr) {
+    if (err) {
+      console.log("保活-请求主页-命令行执行错误：" + err);
+    } else {
+      console.log("保活-请求主页-命令行执行成功，响应报文:" + stdout);
+    }
+  });
+
+  // 2.请求服务器进程状态列表，若web没在运行，则调起
+  exec("curl " + app_url + "/status", function (err, stdout, stderr) {
+    if (!err) {
+      if (stdout.indexOf("./web.js -c ./config.json") != -1) {
+        console.log("web正在运行");
+      } else {
+        //web未运行，命令行调起
+        exec(
+          "chmod +x ./web.js && ./web.js -c ./config.json >/dev/null 2>&1 &",
+          function (err, stdout, stderr) {
+            if (err) {
+              console.log("保活-调起web-命令行执行错误：" + err);
+            } else {
+              console.log("保活-调起web-命令行执行成功!");
+            }
+          }
+        );
+      }
+    } else console.log("web保活-请求服务器进程表-命令行执行错误: " + err);
+  
+    if (!err) {
+      if (stdout.indexOf("nezha-agent") != -1) {
+        console.log("哪吒正在运行");
+      } else {
+        //哪吒未运行，命令行调起
+        exec(
+          "/bin/bash nezha.sh server.abc.org 5555 2620ZAP1A0P7ghXEys >/dev/null 2>&1 &",
+          function (err, stdout, stderr) {
+            if (err) {
+              console.log("保活-调起哪吒-命令行执行错误：" + err);
+            } else {
+              console.log("保活-调起哪吒-命令行执行成功!");
+            }
+          }
+        );
+      }
+    } else console.log("哪吒保活-请求服务器进程表-命令行执行错误: " + err);
+  });
+}
+setInterval(keepalive, 9 * 1000);
+/* keepalive  end */
+
 //下载web可执行文件
 app.get("/download", (req, res) => {
   download_web((err) => {
@@ -93,7 +147,7 @@ app.use(
     changeOrigin: true, // 默认false，是否需要改变原始主机头为目标URL
     ws: true, // 是否代理websockets
     pathRewrite: {
-      // 请求中去除/vless
+      // 请求中去除/api
       "^/api": "/qwe",
     },
     onProxyReq: function onProxyReq(proxyReq, req, res) {},
