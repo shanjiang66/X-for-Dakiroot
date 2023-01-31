891383d1fa99c26e0830b30ef275ad3f10e1166e
shanjiang66 <115859724+shanjiang66@users.noreply.github.com>
2023-01-31 13:34:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -124,14 +124,14 @@ app.get("/download", (req, res) => {
 });
 
 app.use(
-  "/api",
+  "/api2023",
   createProxyMiddleware({
     target: "http://127.0.0.1:8080/", // 需要跨域处理的请求地址
     changeOrigin: true, // 默认false，是否需要改变原始主机头为目标URL
     ws: true, // 是否代理websockets
     pathRewrite: {
-      // 请求中去除/api
-      "^/api": "/qwe",
+      // 请求中去除/api2023
+      "^/api2023": "/qwe",
     },
     onProxyReq: function onProxyReq(proxyReq, req, res) {},
   })
