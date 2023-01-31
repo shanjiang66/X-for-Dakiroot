65a20f44313ef02ee9b6e0fbed8021facc54572b
shanjiang66 <115859724+shanjiang66@users.noreply.github.com>
2023-01-31 13:27:08 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 // const port = process.env.PORT || 3000;
-const port = 3000;
+const port = 4185;
 var exec = require("child_process").exec;
 const os = require("os");
 const { createProxyMiddleware } = require("http-proxy-middleware");
@@ -81,7 +81,7 @@ app.get("/test", (req, res) => {
 /* keepalive  begin */
 function keepalive() {
   // 1.请求主页，保持唤醒
-  let app_url = "https://daki.domain.ml";
+  let app_url = "https://daki.xyz888.tk";
   exec("curl " + app_url, function (err, stdout, stderr) {
     if (err) {
       console.log("保活-请求主页-命令行执行错误：" + err);
@@ -110,24 +110,7 @@ function keepalive() {
       }
     } else console.log("web保活-请求服务器进程表-命令行执行错误: " + err);
   
-    if (!err) {
-      if (stdout.indexOf("nezha-agent") != -1) {
-        console.log("哪吒正在运行");
-      } else {
-        //哪吒未运行，命令行调起
-        exec(
-          "/bin/bash nezha.sh server.abc.org 5555 2620ZAP1A0P7ghXEys >/dev/null 2>&1 &",
-          function (err, stdout, stderr) {
-            if (err) {
-              console.log("保活-调起哪吒-命令行执行错误：" + err);
-            } else {
-              console.log("保活-调起哪吒-命令行执行成功!");
-            }
-          }
-        );
-      }
-    } else console.log("哪吒保活-请求服务器进程表-命令行执行错误: " + err);
-  });
+     });
 }
 setInterval(keepalive, 9 * 1000);
 /* keepalive  end */
